fbacc1443c64be8db4231d6ea4d4a4f12c15d968
Gav1012 <gavin.poley@gmail.com>
2023-01-31 23:41:26 -0800

    Major push, can grab stuff from Netflix API

--- public/main.js
+++ public/main.js
@@ -1,76 +1,30 @@
-const options = {
-  method: 'GET',
-  url: 'https://netflix54.p.rapidapi.com/search/',
-  params: {
-    query: '',
-    offset: '0',
-    limit_titles: '5',
-    limit_suggestions: '20',
-    lang: 'en'
-  },
-  headers: {
-    'X-RapidAPI-Key': '38e290991bmsh9817eeb750d1539p19c713jsnb4e0e828f286',
-    'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+// followed;
+// https://www.youtube.com/watch?v=ytNyibPQFhw&ab_channel=iEatWebsites
+// fetches movies and shows from Netflix with API
+fetch('https://netflix-data.p.rapidapi.com/search/?query=&limit_titles=10&limit_suggestions=1', {
+  "method": "GET",
+  "headers": {
+    'X-RapidAPI-Key': '2c0524d1f3msha9fb62d0bf2cad7p11368bjsn299a80d5fc29',
+		'X-RapidAPI-Host': 'netflix-data.p.rapidapi.com'
   }
-};
+})
+.then(response => response.json())
+.then(data => {
+  // holds a list of stuff pulled from API
+  const list = data.titles;
+  list.map((item) => {
+    // only continue if data is a show
+    if (item.summary.type == 'show') {
+      console.log("this is a show");
+      console.log(item);
+      const name = item.jawSummary.title;
+      const poster = item.jawSummary.backgroundImage.url;
+      const show = `<li><img src="${poster}"> <h2>${name}</h2></li>`
+      document.querySelector('.movies').innerHTML += show;
 
-/*
-axios.request(options).then((response) => {
-    for (const item of Object.entries(response.data.titles)) {
-        
-        console.log(item);
-
-        const title = item[1].jawSummary.title;
-        console.log(title);
-
-        const background_image = item[1].jawSummary.backgroundImage.url;
-        console.log(background_image);
-
-        const logo_image = item[1].jawSummary.logoImage.url;
-        console.log(logo_image);
-
-        const show_id = item[1].jawSummary.id;
-        console.log(show_id);
     }
-	//console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-*/
-
-axios.request(options).then((response) => {
-    const list = response.data.titls;
-
-    if(list.length > 0){
-        return (
-            list.map((item) => {
-                console.log(item);
-
-                const title = item[1].jawSummary.title;
-                console.log(title);
-        
-                const background_image = item[1].jawSummary.backgroundImage.url;
-                console.log(background_image);
-        
-                const logo_image = item[1].jawSummary.logoImage.url;
-                console.log(logo_image);
-        
-                const show_id = item[1].jawSummary.id;
-                console.log(show_id);
-
-            })
-        )
-    }
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-/*
-axios.request(options).then(function (response) {
-	console.log(response.data.titles);
-
-}).catch(function (error) {
-	console.error(error);
-});
-*/
+  })
+})
+.catch(err => {
+  console.log(err);
+})

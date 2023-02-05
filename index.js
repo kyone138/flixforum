1ba5498e6b91130bb54b8bc570447e97dd57dfbe
Julia Tober <jtober@ucsc.edu>
2023-02-04 22:06:33 -0800

    changed local host db to aws database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const { getDefaultNormalizer } = require("@testing-library/react");
+const { Connection } = require("pg");
 
 const app = express();
 
@@ -10,12 +11,19 @@ app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'password',
-    database: 'LoginSystem',
+    user: 'admin',
+    host: 'flixforum-db.cdwyjlv3wddo.us-west-2.rds.amazonaws.com',
+    password: 'FlixForum',
+    port: '3306',
+    //database: 'LoginSystem',
 });
 
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    db.end();
+});
+/*
 app.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -58,3 +66,4 @@ app.post("/login", (req, res) => {
 app.listen(3001, () => {
     console.log("running server");
 })
+*/
